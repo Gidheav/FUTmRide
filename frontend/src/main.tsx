db413cf866c8ade043ffdb2b032783767a8d67d0
GidHeav <Adeniran.m2302417@st.futminna.edu.ng>
2026-07-03 05:47:17 +0100

    .

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'react-hot-toast'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import ErrorBoundary from './core/components/ErrorBoundary'
 import './index.css'
 import AppRouter from './core/AppRouter'
@@ -29,6 +30,7 @@ if (rootElement && !desktopOnlyBlocked) {
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
             <AppRouter />
+            <SpeedInsights />
             <Toaster
               position="top-right"
               toastOptions={{
