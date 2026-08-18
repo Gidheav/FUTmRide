6e300cd7ff524e69f4114de24436c3f57e9b5556
GidHeav <Adeniran.m2302417@st.futminna.edu.ng>
2026-08-18 09:46:24 +0100

    .

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -167,6 +167,7 @@ CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[
     "http://127.0.0.1:5174",
     "http://127.0.0.1:5175",
     "http://127.0.0.1:8002",
+    "http://localhost:4173"
 ])
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
